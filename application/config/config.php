ca11bfdb68caab68fafb9c4110abb21f383eea27
Bayu Aji <bayu.xiii@gmail.com>
2017-04-03 13:00:35 +0700

    Config + database

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://rsa.apps.undip.ac.id/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
