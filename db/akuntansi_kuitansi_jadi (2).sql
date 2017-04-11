f272e856613d85a114534fba0fa577e392e5c26c
Bayu Aji <bayu.xiii@gmail.com>
2017-04-11 11:32:04 +0700

    penambahan status di tabel akuntansi_kuitansi_jadi
    tambah kolom status (enum) di tabel akuntansi_kuitansi_jadi
    1 = proses
    2 = revisi
    3 = posted

---
+++ db/akuntansi_kuitansi_jadi (2).sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.4
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 11, 2017 at 06:29 AM
+-- Server version: 5.6.15-log
+-- PHP Version: 5.5.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `rsa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akuntansi_kuitansi_jadi`
+--
+
+CREATE TABLE IF NOT EXISTS `akuntansi_kuitansi_jadi` (
+  `id_kuitansi_jadi` int(11) NOT NULL AUTO_INCREMENT,
+  `id_kuitansi` int(11) NOT NULL,
+  `no_bukti` varchar(50) NOT NULL,
+  `no_spm` varchar(150) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jenis` varchar(50) NOT NULL,
+  `kode_kegiatan` varchar(100) NOT NULL,
+  `unit_kerja` int(11) NOT NULL,
+  `uraian` text NOT NULL,
+  `jenis_pembatasan_dana` varchar(100) NOT NULL,
+  `akun_debet` varchar(10) NOT NULL,
+  `akun_debet_akrual` varchar(10) NOT NULL,
+  `jumlah_debet` bigint(20) NOT NULL,
+  `akun_kredit` varchar(10) NOT NULL,
+  `akun_kredit_akrual` varchar(10) NOT NULL,
+  `jumlah_kredit` bigint(20) NOT NULL,
+  `flag` int(2) NOT NULL,
+  `status` enum('proses','revisi','posted','') NOT NULL,
+  PRIMARY KEY (`id_kuitansi_jadi`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
