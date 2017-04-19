08add0dc870585c54658d73f54cf7567d0a93d5d
firhan200 <firhan.faisal1995@gmail.com>
2017-04-20 05:30:26 +0700

    ganti label

--- application/views/akuntansi/memorial_tambah.php
+++ application/views/akuntansi/memorial_tambah.php
@@ -7,7 +7,7 @@
 <div class="row">
   <ol class="breadcrumb">
     <li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
-    <li class="active">Penerimaan</li>
+    <li class="active">Memorial</li>
   </ol>
 </div><!--/.row-->
 
