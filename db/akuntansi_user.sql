168cb1ed4e5786f698120d24c6bd3dc45bc94804
firhan200 <firhan.faisal1995@gmail.com>
2017-05-04 11:15:17 +0700

    db baru

--- db/akuntansi_user.sql
+++ db/akuntansi_user.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2017 at 08:39 AM
+-- Generation Time: May 04, 2017 at 06:14 AM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -31,17 +31,21 @@ CREATE TABLE IF NOT EXISTS `akuntansi_user` (
   `username` varchar(100) NOT NULL,
   `password` varchar(60) NOT NULL,
   `level` int(1) NOT NULL,
+  `kode_unit` varchar(10) NOT NULL,
+  `kode_user` varchar(50) NOT NULL,
   `aktif` int(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `akuntansi_user`
 --
 
-INSERT INTO `akuntansi_user` (`id`, `username`, `password`, `level`, `aktif`) VALUES
-(1, 'operator', 'fe96dd39756ac41b74283a9292652d366d73931f', 1, 1),
-(2, 'verifikator', '89419c696ec663c93d47106d1d77841dd23be4d5', 2, 1),
-(3, 'posting', '77f0dc34eb53c77ac74f359def94bcab7138398a', 3, 1);
+INSERT INTO `akuntansi_user` (`id`, `username`, `password`, `level`, `kode_unit`, `kode_user`, `aktif`) VALUES
+(1, 'operator', 'fe96dd39756ac41b74283a9292652d366d73931f', 1, '', '', 1),
+(2, 'verifikator', '89419c696ec663c93d47106d1d77841dd23be4d5', 2, '', 'X2', 1),
+(3, 'universitas', '223be9d546a4de0ec20c80f3935d82a0171f793f', 3, '', '', 1),
+(4, '11', '17ba0791499db908433b80f37c5fbc89b870084b', 1, '11', '', 1),
+(5, '12', '7b52009b64fd0a2a49e6d8a939753077792b0554', 1, '12', '', 1);
 
 --
 -- Indexes for dumped tables
@@ -61,7 +65,7 @@ ALTER TABLE `akuntansi_user`
 -- AUTO_INCREMENT for table `akuntansi_user`
 --
 ALTER TABLE `akuntansi_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
