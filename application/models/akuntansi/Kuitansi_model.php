7f520ee27c2cb47336d13a99966d1913b5b71b2e
firhan200 <firhan.faisal1995@gmail.com>
2017-06-02 04:38:04 +0700

    tampilan kuitansi list

--- application/models/akuntansi/Kuitansi_model.php
+++ application/models/akuntansi/Kuitansi_model.php
@@ -55,10 +55,10 @@ class Kuitansi_model extends CI_Model {
         }
 
         if($limit!=null OR $start!=null){
-            $query = $this->db->query("SELECT * FROM akuntansi_kuitansi_jadi WHERE jenis='GP' AND tipe!='memorial' AND tipe!='jurnal_umum' AND  
+            $query = $this->db->query("SELECT * FROM akuntansi_kuitansi_jadi WHERE jenis='GP' AND (tipe<>'memorial' AND tipe<>'jurnal_umum' AND tipe<>'pajak') AND  
             (no_bukti LIKE '%$keyword%' OR no_spm LIKE '%$keyword%') $unit ORDER BY FIELD(status, 'revisi', 'terima', 'proses', 'posted') LIMIT $start, $limit");
         }else{
-            $query = $this->db->query("SELECT * FROM akuntansi_kuitansi_jadi WHERE jenis='GP' AND tipe!='memorial' AND tipe!='jurnal_umum' AND  
+            $query = $this->db->query("SELECT * FROM akuntansi_kuitansi_jadi WHERE jenis='GP' AND (tipe<>'memorial' AND tipe<>'jurnal_umum' AND tipe<>'pajak') AND  
             (no_bukti LIKE '%$keyword%' OR no_spm LIKE '%$keyword%') $unit ORDER BY FIELD(status, 'revisi', 'terima', 'proses', 'posted')");
         }
         return $query;
