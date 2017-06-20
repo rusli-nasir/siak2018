c8f4fe1e9e6d08e2ac475e55e2f4e739b27a4a81
firhan200 <firhan.faisal1995@gmail.com>
2017-06-20 21:03:11 +0700

    filter spm di verifikator & biaya ganti beban

--- application/views/akuntansi/kuitansi_lsphk3_list.php
+++ application/views/akuntansi/kuitansi_lsphk3_list.php
@@ -93,31 +93,17 @@ tbody td, thead th {
 </div><!--/.row-->
 <div class="row">
 	<div class="col-sm-4">
-		<?php if(isset($tab1)){ ?>
-		<form action="<?php echo site_url('akuntansi/kuitansi/jadi'); ?>" method="post">
-			<div class="input-group">
-				<span class="input-group-btn">
-	        		<a href="<?php echo site_url('akuntansi/kuitansi/reset_search_jadi'); ?>"><button class="btn btn-danger" type="button"><span class="glyphicon glyphicon-refresh"></span> Reset</button></a>
-	      		</span>
-	      		<input type="text" class="form-control" placeholder="No.bukti/No.SPM/Uraian" name="keyword_jadi" value="<?php if($this->session->userdata('keyword_jadi')) echo $this->session->userdata('keyword_jadi'); ?>">
-	      		<span class="input-group-btn">
-	        		<button class="btn btn-default" type="submit">Cari</button>
-	      		</span>
-	    	</div>
-	    </form>
-	    <?php }else{ ?>
-	    <form action="<?php echo site_url('akuntansi/kuitansi/index_ls'); ?>" method="post">
+	    <form action="<?php echo site_url('akuntansi/kuitansi/jadi_ls'); ?>" method="post">
 			<div class="input-group">
 				<span class="input-group-btn">
 	        		<a href="<?php echo site_url('akuntansi/kuitansi/reset_search_jadi_ls'); ?>"><button class="btn btn-danger" type="button"><span class="glyphicon glyphicon-refresh"></span> Reset</button></a>
 	      		</span>
-	      		<input type="text" class="form-control" placeholder="No.bukti/No.SPM/Uraian" name="keyword_ls" value="<?php if($this->session->userdata('keyword_ls')) echo $this->session->userdata('keyword_ls'); ?>">
+	      		<input type="text" class="form-control" placeholder="No.bukti/No.SPM" name="keyword_jadi_ls" value="<?php if($this->session->userdata('keyword_jadi_ls')) echo $this->session->userdata('keyword_jadi_ls'); ?>">
 	      		<span class="input-group-btn">
 	        		<button class="btn btn-default" type="submit">Cari</button>
 	      		</span>
 	    	</div>
 	    </form>
-	    <?php } ?>
 	</div>
 	<?php if($this->session->userdata('level')=='2' AND $this->session->userdata('username')=='verifikator'){ ?>
 	<div class="col-sm-4">	
