c16cb5e9c1ae1d0e637565b4a923cf528f81f7e1
Muhammad Haiqal Fuadi <haiqal.fuadi@gmail.com>
2018-01-16 10:12:48 +0700

    import excel anggaran

--- application/controllers/akuntansi/Anggaran.php
+++ application/controllers/akuntansi/Anggaran.php
@@ -8,6 +8,8 @@ class Anggaran extends MY_Controller {
 		parent::__construct();
 		$this->cek_session_in();
       $this->load->model('akuntansi/Anggaran_model', 'Anggaran_model');	
+      $this->load->model('akuntansi/Unit_kerja_model', 'Unit_kerja_model');	
+      $this->db2 = $this->load->database('rba',TRUE);
 	}
 
 	public function index()
@@ -23,6 +25,73 @@ class Anggaran extends MY_Controller {
 	}
 
 	public function do_upload_pendapatan_temporer()
+	{
+		$this->load->library('excel');
+		$config['upload_path'] = './assets/akuntansi/upload';
+		$config['allowed_types'] = 'xls|xlsx';
+		$config['max_size'] = '20000';
+		PHPExcel_Settings::setZipClass(PHPExcel_Settings::PCLZIP);
+
+		$this->load->library('upload', $config);
+
+		if ( ! $this->upload->do_upload())
+		{
+			echo $this->upload->display_errors('<p>', '</p>');
+			die('gagal mengupload');
+		}
+
+		$data = $this->upload->data();
+
+		$file = $data['full_path'];
+
+		$inputFileType = PHPExcel_IOFactory::identify($file);
+
+		$objReader = PHPExcel_IOFactory::createReader($inputFileType);
+		$objReader->setReadDataOnly(true);
+		$objPHPExcel = $objReader->load($file);
+
+
+		$data = array();
+
+		$i = 0;
+		$objPHPExcel->setActiveSheetIndex($i);
+		$sheet = $objPHPExcel->getSheet($i); 
+		$objWorksheet = $objPHPExcel->getActiveSheet();
+            	$highest_row = $objWorksheet->getHighestRow(); // e.g. 10
+            	// print_r($highest_row);die();
+            	$tahun = $this->session->userdata('setting_tahun'); 	
+            	$start_row = 5;
+
+            	for ($row=$start_row; $row <= $highest_row-1; $row++) { 	
+            		if ( $objWorksheet->getCellByColumnAndRow(2,$row)->getValue() == '') {
+            			$unit = $objWorksheet->getCellByColumnAndRow(1,$row)->getValue(); 
+            			$kode_unit = $this->get_unit($unit);
+            			$row++;
+            		}
+            		$akun = $objWorksheet->getCellByColumnAndRow(2,$row)->getValue();
+            		$anggaran = $objWorksheet->getCellByColumnAndRow(4,$row)->getValue();
+
+            		$data = array(
+            			'kode_unit' =>  $kode_unit,
+            			'akun' =>  $akun,
+            			'anggaran' =>  $anggaran,
+            			'tahun' =>  $tahun,
+            			'jenis_pembatasan_dana' =>  'terikat_temporer',
+            		);
+            		$this->Anggaran_model->upload_pendapatan_temporer($data);
+            	}
+
+            	$this->session->set_flashdata('success', 'Data berhasil diterima!');
+            	redirect('/akuntansi/Anggaran/upload_pendapatan_temporer/');
+            //  echo "<pre>";
+
+            // print_r($data);die();
+
+            // unlink($file);
+            // die($file);
+    }
+
+	public function do_upload_pendapatan_temporerx()
     {
     		$this->load->library('excel');
             $config['upload_path'] = './assets/akuntansi/upload';
@@ -93,6 +162,18 @@ class Anggaran extends MY_Controller {
 
             // unlink($file);
             // die($file);
+    }
+
+    public function get_unit($unit){
+    	$query = $this->Unit_kerja_model->get_all_unit_kerja();
+    	foreach ($query as $data) {
+    		if ($unit == $data['alias'] or $unit == $data['nama_unit']) {
+    			$unit = $data['kode_unit'];
+    		}else{
+    			$unit = NULL;
+    		}
+    	}
+    	return $unit;
     }
 
 }
